1baa9d98de694cf30ae7e057f16b576bfaab4540
vikkiakavc <vschaudhary2001@gmail.com>
2023-12-07 22:58:06 +0530

    add various changes again

--- src/db/index.js
+++ src/db/index.js
@@ -1,8 +1,8 @@
 const { Sequelize, DataTypes } = require('sequelize');
 
-const sequelize = new Sequelize('project1', 'root', '', {
-    host: '127.0.0.1',
-    port: 3306,
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: 'mysql',
     pool: {
         max: 5,
@@ -10,7 +10,7 @@ const sequelize = new Sequelize('project1', 'root', '', {
         acquire: 30000,
         idle: 10000
     }
-})
+});
 
 sequelize.authenticate().then(() => {
     console.log('connected')
