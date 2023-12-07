a633446262703ccc9716aa0edfd593be414afaac
vikkiakavc <vschaudhary2001@gmail.com>
2023-12-07 23:11:41 +0530

    add various changes again

--- src/db/index.js
+++ src/db/index.js
@@ -1,8 +1,8 @@
 const { Sequelize, DataTypes } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+const sequelize = new Sequelize(process.env.DB_NAME || "project1", process.env.DB_USER || "root", process.env.DB_PASSWORD || "", {
+    host: process.env.DB_HOST || "127.0.0.1",
+    port: process.env.DB_PORT || 3306,
     dialect: 'mysql',
     pool: {
         max: 5,
@@ -25,7 +25,7 @@ db.sequelize = sequelize;
 db.shorts = require('../models/shorts.js')(sequelize, DataTypes)
 
 
-db.sequelize.sync({ force: false }).then(() => {
+db.sequelize.sync({ force: true }).then(() => {
     console.log(' yes re-sync')
 })
 
