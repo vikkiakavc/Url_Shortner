22479b500aa0cd58f6d7d235edc5d25a0c797e4d
vikkiakavc <vschaudhary2001@gmail.com>
2023-12-08 16:00:12 +0530

    change back to previos saved version

--- src/db/index.js
+++ src/db/index.js
@@ -1,9 +1,16 @@
 const { Sequelize, DataTypes } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.MYSQL_DATABASE_URL, {
+const sequelize = new Sequelize(process.env.DB_NAME || "project1", process.env.DB_USER || "root", process.env.DB_PASSWORD || "", {
+    host: process.env.DB_HOST || "127.0.0.1",
+    port: process.env.DB_PORT || 3306,
     dialect: 'mysql',
-    logging: false, // Set to true to display SQL queries in the console
-  });
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+});
 
 sequelize.authenticate().then(() => {
     console.log('connected')
